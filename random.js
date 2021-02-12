2cdab5e8c329055d5d5f58822ae8742811e8d6c9
Rizalanggun <78941302+Rizalanggun@users.noreply.github.com>
2021-02-13 04:10:53 +0700

    Create random.js

---
+++ random.js
@@ -0,0 +1,15 @@
+<script type='text/javascript'>
+//javascript quote in arrays db
+quote = new Array(5);
+quote[0] = "<b>Jancuk1</b>";
+quote[1] = "<b>Jancuk2</b>";
+quote[2] = "<b>Jancuk3</b>";
+quote[3] = "<b>Jancuk4</b>";
+quote[4] = "<b>Jancuk5</b>";
+//menentukan random index
+index = Math.floor(Math.random() * quote.length);
+//menampilkan quote
+document.write("\n");
+document.write(quote[index]);
+//db end arrays
+</script>
